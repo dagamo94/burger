f3451bf9643069c43e5ceea62453e847aa2c234b
Surface <da.ga.mo.94@gmail.com>
2019-07-12 22:05:33 -0600

    Final version with styling

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -24,23 +24,25 @@ $(function() {
   $(".create-form").on("submit", function(event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
-
-    var newburger = {
-      name: $("#ca").val().trim()
-      // devour: $("[name=devour]:checked").val().trim()
-    };
-
-    // Send the POST request.
-    $.ajax("/api/burgers", {
-      type: "POST",
-      data: newburger
-    }).then(
-      function() {
-        console.log("created new burger");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
+    if($("#ca").val() !== ""){
+      
+      var newburger = {
+        name: $("#ca").val().trim()
+        // devour: $("[name=devour]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newburger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    }
   });
 
   $(".delete-burger").on("click", function(event) {
